1103b5c46ba066eed3eddab6050fd76eb9cd264c
Desti <apella2010@gmail.com>
2018-05-17 11:18:05 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "newappr_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'testairbnb111@gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            'testairbnb111@gmail.com',
+  password:             ENV['Gpas'],
+  authentication:       'plain',
+  enable_starttls_auto: true }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
